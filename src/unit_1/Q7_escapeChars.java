887de03608e22687a2af4173b2ee721c20951082
Kemar Hinds <40723880@live.napier.ac.uk>
2025-06-04 07:59:47 -0400

    Readme update

--- src/unit_1/Q7_escapeChars.java
+++ src/unit_1/Q7_escapeChars.java
@@ -4,8 +4,7 @@ import javax.swing.JOptionPane;
 public class Q7_escapeChars {
     public static void main(String[] args) {
 
-        // Corrected escape for backslash
-        String output = "Hello this is \\\\";
+
 
         // Showing the message for backslash
         JOptionPane.showMessageDialog(null, "The backslash character (\\) is easy to print");
