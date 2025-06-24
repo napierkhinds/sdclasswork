2dca96c391850834a6dfaea5fe2818f744bc4858
Kemar Hinds <40723880@live.napier.ac.uk>
2025-06-24 13:02:29 -0400

    Unit 5 update

---
+++ src/unit_5/Q8_name_address.java
@@ -0,0 +1,68 @@
+package unit_5;
+
+import javax.swing.JOptionPane;
+
+public class Q8_name_address {
+    public static void main (String[] args){
+
+        String choiceAsString;
+        int choice;
+        String name, address;
+        String ageAsString;
+        int age;
+        String menue = " Welcom to Personal Data Printer \n"
+                        + "1. Name + Address + Age \n"
+                        + "2. Name + Address \n"
+                        + "3. Name + Age \n";
+
+        choiceAsString= JOptionPane.showInputDialog(null, menue);
+        choice = Integer.parseInt(choiceAsString);
+
+        switch(choice){
+            case 1:
+
+                name = JOptionPane.showInputDialog(null, "Enter your name");
+                address = JOptionPane.showInputDialog(null, "Enter your address");
+                ageAsString = JOptionPane.showInputDialog(null, "Enter your age");
+                age = Integer.parseInt(ageAsString);
+
+                printDetails(name,address,age);
+                break;
+
+                case 2:
+                name = JOptionPane.showInputDialog(null, "Enter your name");
+                address = JOptionPane.showInputDialog(null, "Enter your address");
+
+                printDetails(name,address);
+                break;
+
+                case 3:
+                name = JOptionPane.showInputDialog(null, "Enter your name");
+                ageAsString = JOptionPane.showInputDialog(null, "Enter your age");
+                age = Integer.parseInt(ageAsString);
+
+                printDetails(name,age);
+        }
+
+
+
+
+
+    }// end main
+
+    public static void printDetails(String name, String address, int age) {
+
+        JOptionPane.showMessageDialog(null, "Name : " + name + "\nAddress : " + address + "\nAge : " + age);
+    }
+
+    public static void printDetails(String name,String address){
+
+        JOptionPane.showMessageDialog(null,"Name : "+name+"\nAddress : "+address);
+    }
+
+    public static void printDetails(String name,int age){
+
+        JOptionPane.showMessageDialog(null,"Name : "+name+"\nAge : "+age);
+    }
+
+}// end class
