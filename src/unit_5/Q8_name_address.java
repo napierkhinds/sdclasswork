cfcbaf25c9651e75b50251b3c3c8ecbeafc4016c
Kemar Hinds <40723880@live.napier.ac.uk>
2025-06-24 13:11:24 -0400

    Unit 5 update

--- src/unit_5/Q8_name_address.java
+++ src/unit_5/Q8_name_address.java
@@ -10,41 +10,51 @@ public class Q8_name_address {
         String name, address;
         String ageAsString;
         int age;
-        String menue = " Welcom to Personal Data Printer \n"
+        String menu = " Welcome to Personal Data Printer \n"
                         + "1. Name + Address + Age \n"
                         + "2. Name + Address \n"
-                        + "3. Name + Age \n";
-
-        choiceAsString= JOptionPane.showInputDialog(null, menue);
+                        + "3. Name + Age \n"
+                        + "4. Exit";
+        do {
+        choiceAsString= JOptionPane.showInputDialog(null, menu);
         choice = Integer.parseInt(choiceAsString);
 
-        switch(choice){
-            case 1:
+    switch (choice) {
+        case 1:
 
-                name = JOptionPane.showInputDialog(null, "Enter your name");
-                address = JOptionPane.showInputDialog(null, "Enter your address");
-                ageAsString = JOptionPane.showInputDialog(null, "Enter your age");
-                age = Integer.parseInt(ageAsString);
+            name = JOptionPane.showInputDialog(null, "Enter your name");
+            address = JOptionPane.showInputDialog(null, "Enter your address");
+            ageAsString = JOptionPane.showInputDialog(null, "Enter your age");
+            age = Integer.parseInt(ageAsString);
 
-                printDetails(name,address,age);
-                break;
+            printDetails(name, address, age);
+            break;
 
-                case 2:
-                name = JOptionPane.showInputDialog(null, "Enter your name");
-                address = JOptionPane.showInputDialog(null, "Enter your address");
+        case 2:
+            name = JOptionPane.showInputDialog(null, "Enter your name");
+            address = JOptionPane.showInputDialog(null, "Enter your address");
 
-                printDetails(name,address);
-                break;
+            printDetails(name, address);
+            break;
 
-                case 3:
-                name = JOptionPane.showInputDialog(null, "Enter your name");
-                ageAsString = JOptionPane.showInputDialog(null, "Enter your age");
-                age = Integer.parseInt(ageAsString);
+        case 3:
+            name = JOptionPane.showInputDialog(null, "Enter your name");
+            ageAsString = JOptionPane.showInputDialog(null, "Enter your age");
+            age = Integer.parseInt(ageAsString);
 
-                printDetails(name,age);
-        }
+            printDetails(name, age);
 
+            break;
 
+        case 4:
+                //JOptionPane.showMessageDialog(null, "Goobye!");
+            break;
+
+        default:
+            JOptionPane.showMessageDialog(null, "Invalid choice, please try again");
+    }
+
+} while (choice != 4);
 
 
 
